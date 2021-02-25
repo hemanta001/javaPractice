3605d3dc05b31c8b61d1f3f89c41c8b7c046e191
Heman Ghimire <rockingguyheman.hg@gmail.com>
2021-02-25 22:10:16 +0545

    method overriding

--- src/InheritanceExample/Rectangle.java
+++ src/InheritanceExample/Rectangle.java
@@ -1,5 +1,7 @@
 package InheritanceExample;
 
+import java.util.Scanner;
+
 public class Rectangle extends Shapes{
 
     public void setProperties(){
@@ -9,5 +11,11 @@ public class Rectangle extends Shapes{
 
     public void area(){
         System.out.println("The area of rectangle is "+ getLength()*getBreadth());
+    }
+
+    public void setLength(){
+        System.out.println("Enter length by overriding method");
+        Scanner scanner=new Scanner(System.in);
+        length=scanner.nextInt();
     }
 }
