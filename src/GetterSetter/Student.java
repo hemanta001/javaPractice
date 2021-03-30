11fe6a213ca7bf462dcdcdc149e3be16719b102e
Heman Ghimire <rockingguyheman.hg@gmail.com>
2021-03-30 20:48:11 +0545

    getter setter auto create

--- src/GetterSetter/Student.java
+++ src/GetterSetter/Student.java
@@ -5,44 +5,46 @@ public class Student {
     private String firstName;
     private String lastName;
     String gender;
-    int rollNumber;
-    int age;
+    private int rollNumber;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public  void setGender(String gender){
-        this.gender= gender;
-    }
-    public  void setRollNumber(int rollNumber){
-        this.rollNumber=rollNumber;
-
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getLastName(){
+    public String getLastName() {
         return lastName;
     }
-    public String getFirstName(){
-        return firstName;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getGender() {
         return gender;
     }
 
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public int getRollNumber() {
         return rollNumber;
     }
 
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
     public int getAge() {
         return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
