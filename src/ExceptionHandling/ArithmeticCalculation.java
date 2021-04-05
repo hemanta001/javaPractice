2cb65aa497b55d197936334d78531561dc6010d6
Heman Ghimire <rockingguyheman.hg@gmail.com>
2021-04-05 20:56:26 +0545

    catch exception java

--- src/ExceptionHandling/ArithmeticCalculation.java
+++ src/ExceptionHandling/ArithmeticCalculation.java
@@ -1,16 +1,25 @@
 package ExceptionHandling;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArithmeticCalculation {
 
     public void divide(){
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter numerator");
-        int numerator=scanner.nextInt();
-        System.out.println("Enter denominator");
-        int denominator=scanner.nextInt();
-        divideOperation(numerator,denominator,scanner);
+        int numerator;
+        int denominator;
+        try{
+            System.out.println("Enter numerator");
+            numerator=scanner.nextInt();
+            System.out.println("Enter denominator");
+            denominator=scanner.nextInt();
+            divideOperation(numerator,denominator,scanner);
+        }
+        catch (InputMismatchException e){
+            divide();
+        }
+
     }
 
     public void divideOperation(int numerator,int denominator,Scanner scanner){
@@ -18,7 +27,7 @@ public class ArithmeticCalculation {
             int quotient=numerator/denominator;
             System.out.println("The quotinet is "+quotient);
         }
-        catch (Exception e){
+        catch (ArithmeticException e){
             System.out.println("Enter denominator. Denominator cannot be zero");
             denominator=scanner.nextInt();
             divideOperation(numerator,denominator,scanner);
