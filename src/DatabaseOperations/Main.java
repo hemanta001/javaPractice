b2bde804fa7e86c6787876b39c7df60aae5e9c44
Hemanta Ghimire <rockingguyheman.hg@gmail.com>
2021-09-09 20:49:26 +0545

    read user from database

---
+++ src/DatabaseOperations/Main.java
@@ -0,0 +1,7 @@
+package DatabaseOperations;
+
+public class Main {
+    public static void main(String[] args) {
+        new UserService().readUsers();
+    }
+}
---
+++ src/DatabaseOperations/UserService.java
@@ -0,0 +1,31 @@
+package DatabaseOperations;
+
+import mysqlConnector.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UserService {
+
+    public void readUsers() {
+        try {
+            String query="select * from user";
+            Connection connection=new DatabaseConnection().connectToDB();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                //Display values
+                System.out.print("ID: " + rs.getLong("id"));
+                System.out.print(", FIRSTNAME: " + rs.getString("firstName"));
+                System.out.print(", LASTNAME: " + rs.getString("lastName"));
+                System.out.println(", EMAIL: " + rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
