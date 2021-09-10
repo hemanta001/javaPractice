1526d20c6182bb281192a74f49df7f9400c07e3b
Hemanta Ghimire <rockingguyheman.hg@gmail.com>
2021-09-10 21:24:57 +0545

    create and read operations java

--- src/DatabaseOperations/Main.java
+++ src/DatabaseOperations/Main.java
@@ -1,7 +1,20 @@
 package DatabaseOperations;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        new UserService().readUsers();
+        UserService userService = new UserService();
+        //Display inserted user
+        User insertedUser = userService.insertUser();
+        System.out.println("-----inserted user is-------");
+        userService.displayUser(insertedUser);
+
+        //Display list of values
+        List<User> userList = userService.readUsers();
+        System.out.println("-----userList-------");
+        for (User user : userList) {
+            userService.displayUser(user);
+        }
     }
 }
--- src/DatabaseOperations/UserService.java
+++ src/DatabaseOperations/UserService.java
@@ -2,32 +2,86 @@ package DatabaseOperations;
 
 import mysqlConnector.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class UserService {
 
-    public void readUsers() {
+    public List<User> readUsers() {
+        List<User> userList = new ArrayList<>();
         try {
-            String query="select * from user";
-            Connection connection=new DatabaseConnection().connectToDB();
+            String query = "select * from user";
+            Connection connection = new DatabaseConnection().connectToDB();
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
+            User user = null;
             while (rs.next()) {
-                //Display values
-                System.out.print("ID: " + rs.getLong("id"));
-                System.out.print(", FIRSTNAME: " + rs.getString("firstName"));
-                System.out.print(", LASTNAME: " + rs.getString("lastName"));
-                System.out.println(", EMAIL: " + rs.getString("email"));
-                System.out.println(", USERNAME: " + rs.getString("username"));
-
+                user = new User();
+                user.setId(rs.getLong("id"));
+                user.setFirstName(rs.getString("firstName"));
+                user.setLastName(rs.getString("lastName"));
+                user.setEmail(rs.getString("email"));
+                user.setUsername(rs.getString("username"));
+                userList.add(user);
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+        }
+        return userList;
+    }
+
+    public User insertUser() {
+        User user = askUserFromKeyBoard();
+        try {
+            Connection connection = new DatabaseConnection().connectToDB();
+            String query = " insert into user (firstName, lastName, email, username)"
+                    + " values (?, ?, ?, ?)";
+            PreparedStatement preparedStmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStmt.setString(1, user.getFirstName());
+            preparedStmt.setString(2, user.getLastName());
+            preparedStmt.setString(3, user.getEmail());
+            preparedStmt.setString(4, user.getUsername());
+            preparedStmt.execute();
+            ResultSet rs = preparedStmt.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getLong(1));
+            }
+            connection.close();
+            return user;
+        } catch (Exception e) {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+            return null;
         }
     }
 
+    public User askUserFromKeyBoard() {
+        User user = new User();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String firstName = scanner.nextLine();
+        user.setFirstName(firstName);
+        System.out.println("Enter last name");
+        String lastName = scanner.nextLine();
+        user.setLastName(lastName);
+        System.out.println("Enter email");
+        String email = scanner.nextLine();
+        user.setEmail(email);
+        System.out.println("Enter username");
+        String username = scanner.nextLine();
+        user.setUsername(username);
+        return user;
+    }
+
+    public void displayUser(User user) {
+        System.out.print("ID: " + user.getId());
+        System.out.print(", FIRSTNAME: " + user.getFirstName());
+        System.out.print(", LASTNAME: " + user.getLastName());
+        System.out.println(", EMAIL: " + user.getEmail());
+        System.out.println(", USERNAME: " + user.getUsername());
+    }
 }
+
+
